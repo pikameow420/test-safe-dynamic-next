d3478e4ea2deafb2e8b4abc1cf1f2c5403833bb5
Advit <advit@multisafe.finance>
2024-08-28 19:37:28 +0530

    Attempt 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 
-import { DynamicWidget } from "@/lib/dynamic";
+import { DynamicWidget, useDynamicContext, WalletList } from "@/lib/dynamic";
 import { BatchTransaction } from "./components";
 import Image from "next/image";
 import { useAccount } from "wagmi";
 
 export default function Main() {
-  const { isConnected } = useAccount();
+  const { isConnected, connector: activeConnector } = useAccount();
+  const { walletConnector, setPrimaryWallet} = useDynamicContext()
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center text-white">
       <div className="flex flex-col items-center justify-center text-center">
@@ -19,6 +20,12 @@ export default function Main() {
         <h1 className="text-4xl font-bold mb-4">Testing Safe with Dynamic - Added Shim </h1>
          <DynamicWidget />
          {isConnected && <BatchTransaction />}
+
+         
+         <p>isConnected: {isConnected.toString()}</p>
+         <p>activeConnector: {activeConnector?.id}</p>
+
+      {activeConnector?.id === "safe" && <button onClick={() => walletConnector?.connect({activeConnector})}>Set Primary Wallet to Safe</button>}
       </div>
     </div>
   );
